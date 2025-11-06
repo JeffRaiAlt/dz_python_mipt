505ebb7008601fa1946a62aab3e02d34b0a5c7e7
JeffRaiAlt <workbifitfokin@gmail.com>
2025-11-07 00:22:54 +0300

    Реализован первый вариант scraper

--- scraper.py
+++ scraper.py
@@ -1,112 +1,211 @@
-import schedule
-import requests
-from bs4 import BeautifulSoup
 import re
 import time
 from pathlib import Path
 
-#url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+import schedule
+import requests
+from bs4 import BeautifulSoup
+
 URL_PREFIX = "https://books.toscrape.com/catalogue/"
-FILE_NAME = "./artifacts/books_data.txt"
+OUTPUT_FILE_NAME = "./artifacts/books_data.txt"
+REQUEST_TIMEOUT = 15
 
 def get_book_data(book_url: str) -> dict:
-    response = requests.get(book_url)
-    if response.status_code == 200:
-        return parse_book(response.text)
-    else:
-        raise Exception("Ошибка ", response.status_code, response.text)
+    """
+    Функция возвращает информацию о книге по её URL
+    вида https://books.toscrape.com...
+    Для этого отправляет HTTP-запрос по указанному адресу страницы книги,
+    извлекает основные данные (название, автора, описание, рейтинг и т.д.)
+    и собирает их в словарь.
+    Args:
+        book_url (str): URL адрес по которому находится описание книги
 
+    Returns:
+        dict: Словарь с полями:
+            - "Title" (str): Название книги.
+            - "Price" (str): Автор книги.
+            - "Description" (str): Описание книги, если доступно.
+            - "Rating" (int): Средняя оценка, если указана.
+            - "Availability" (int): Наличие
+            и дополнительные характеристики из таблицы Product Information
+    """
+    response = requests.get(book_url, timeout=REQUEST_TIMEOUT)
+    response.raise_for_status()
+    return parse_book(response.text)
 
-"""
-Соберите всю информацию, включая 
-название, 
-цену, 
-рейтинг, 
-количество в наличии, 
-описание и 
-дополнительные характеристики из таблицы Product Information. Результат достаточно вернуть в виде словаря.
-"""
 
 def parse_book(text: str) -> dict:
+    """
+    Функция осуществляет непосредственный разбор переданного текста HTML
+    страницы, выделяет основные параметры книги и возвращает их в виде
+    словаря.
+    Args:
+        text: html страницы
+    Returns:
+        dict: Словарь с полями:
+            - "Title" (str): Название книги.
+            - "Price" (str): Стоимость книги.
+            - "Description" (str): Описание книги, если доступно.
+            - "Rating" (int): Средняя оценка, если указана.
+            - "Availability" (int): Наличие
+            и дополнительные характеристики из таблицы Product Information
+    """
     soup = BeautifulSoup(text, "html.parser")
-    book_info = {}
-    article = soup.find("article", attrs={"class": "product_page"})
+    article = soup.find("article",
+                        attrs={"class": "product_page"})
+    if article is None:
+        raise ValueError("Не корректная структура страницы: отсутствует "
+                         "тэг article.")
+    book_info = {"Title": article.find("h1").text.strip()}
 
-    book_info["name"] = article.find("h1").text.strip()
+    price_tag = (article.find("p",
+                          attrs={"class": "price_color"}))
+    # Убираем Â перед ценой
+    price = price_tag.get_text(strip=True).replace("Â", "")
+    book_info["Price"] = price
 
-    book_info["price"] = article.find("p", attrs={"class": "price_color"}).text.strip()
+    rating_tag = article.find("p",
+                              attrs={"class": "star-rating"})
+    rating_map = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
 
-    rating_tag = article.find("p", attrs={"class": "star-rating"})
-    book_info["rating"] = {"One": 1, "Two": 2, "Three": 3,
-                     "Four": 4, "Five": 5}.get(rating_tag.get("class")[1], 0)
-    avlb_text = article.find("p", attrs={"class": "instock availability"}).text.strip()
-    book_info["availability"] = re.search("\\d+", avlb_text).group()
-    description_tag = article.find("div", attrs={"id": "product_description"})
+    if rating_tag and rating_tag.get("class") and len(
+            rating_tag["class"]) > 1:
+        book_info["Rating"] = rating_map.get(rating_tag["class"][1], 0)
+
+    avlb_tag = article.find("p",
+                              attrs={"class": "instock availability"})
+    if avlb_tag:
+        avlb_text = avlb_tag.get_text(strip=True)
+        match = re.search(r"\\d+", avlb_text)
+        if match:
+            book_info["Availability"] = int(match.group())
+
+    description_tag = article.find("div",
+                                   attrs={"id": "product_description"})
     if description_tag:
-        book_info["description"] = description_tag.find_next_sibling('p').text.strip()
+        book_info["Description"] = (description_tag.
+                                    find_next_sibling('p').text.strip())
 
-    extra_table = article.find("table", attrs={"class": "table table-striped"})
+    extra_table = article.find("table",
+                               attrs={"class": "table table-striped"})
+
     rows = extra_table.find_all("tr")
     for row in rows:
+        # Убираем дублирующиеся данные
         if not book_info.get(row.find("th").text.strip(), None):
-            book_info[row.find("th").text.strip()] = row.find("td").text.strip()
+            book_info[row.find("th").text.strip()] = (
+                row.find("td").text.strip()).replace("Â", "")
     return book_info
 
 
 def get_books_urls(book_catalogue_url: str) -> list:
-    response = requests.get(book_catalogue_url)
-    print(book_catalogue_url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
-        section = soup.find("section")
-        rows = section.find_all("h3")
-        books_url = []
-        for row in rows:
-            books_url.append(URL_PREFIX+row.find("a").get("href").strip())
-        return books_url
-    else:
-        raise Exception(f"Ошибка, страница {book_catalogue_url} не найдена",
-                        response.status_code, response.text)
+    """
+    Извлекает ссылки на отдельные книги со страницы каталога.
+    Функция отправляет запрос к странице каталога книг, парсит HTML
+    и собирает все ссылки, ведущие на страницы отдельных книг.
+    Args:
+        book_catalogue_url (str): URL страницы каталога книг.
+    Returns:
+        Список строковых URL, каждый из которых ведёт на страницу книги.
+    """
+    response = requests.get(book_catalogue_url, timeout=REQUEST_TIMEOUT)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
+    section = soup.find("section")
+    rows = section.find_all("h3")
+    books_url = []
+    for row in rows:
+        books_url.append(URL_PREFIX + row.find("a").get("href").strip())
+    return books_url
 
 def write_to_file(about_books: list) -> None:
-    #file_name = "FILE_NAME
-    file_path = Path(FILE_NAME)
-    if file_path.exists():
-        file_path.unlink()
-
-    with open(FILE_NAME, "a", encoding="utf-8") as f:
+    """
+    Запись полученной информации в файл
+    Args:
+        about_books (list): данные о книгах
+    """
+    Path(OUTPUT_FILE_NAME).parent.mkdir(parents=True, exist_ok=True)
+    with open(OUTPUT_FILE_NAME, "w", encoding="utf-8") as f:
         for book_info in about_books:
             for key, value in book_info.items():
                 f.write(f'{key}  {value}')
                 f.write('\n')
 
-def get_page_counts() -> int:
-    url = f"{URL_PREFIX}page-1.html"
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
-        pages_text = soup.find("ul", attrs={"class": "pager"}).text.strip()
-        return int(re.search("of (\\d+)", pages_text).group(1))
-    else:
-        raise IOError("", response.status_code, response.text)
 
-def get_interval(from_page, to_page):
+def get_page_counts() -> int:
+    """
+    Определяет общее количество страниц в каталоге.
+    Функция обращается к первой странице каталога находит элемент,
+    содержащий информацию о количестве страниц, и извлекает это число.
+    Используется при пагинации для последовательного обхода всех
+    страниц каталога.
+    Returns:
+        int: Количество страниц каталога.
+    """
+    url = f"{URL_PREFIX}page-1.html"
+    response = requests.get(url, timeout=REQUEST_TIMEOUT)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
+    pages_text = soup.find("ul",
+                               attrs={"class": "pager"}).text.strip()
+    return int(re.search("of (\\d+)", pages_text).group(1))
+
+
+def get_interval(from_page: int, to_page: int) ->  tuple[int, int]:
+    """
+    Возвращает диапазон страниц для обработки.
+    Функция проверяет корректность границ диапазона (номер начальной
+    и конечной страниц), а если верхняя граница не указана, получает ее
+    запросив информацию с сайта, если нижняя не указана ставим по
+    умолчанию 1.
+    Args:
+        from_page (int): Номер первой страницы (включительно).
+        to_page (int): Номер последней страницы (включительно).
+    Returns:
+        tuple[int, int]: кортеж from_page, to_page.
+    Raises:
+        ValueError: если from_page > to_page или если верхняя граница
+        to_page, превышает максимально доступную
+    """
+    if from_page > to_page:
+        raise ValueError("Ошибка, должно быть from_page <= to_page",
+                         from_page, to_page)
+    max_page_number = get_page_counts()
+    if to_page is not None and to_page > max_page_number:
+        raise ValueError(f"Ошибка, максимальная доступная "
+                         f"страница {max_page_number})")
+
     if from_page is None:
         from_page = 1
     if to_page is None:
-        to_page = get_page_counts()
-    if from_page > to_page:
-        raise Exception("Ошибка", from_page, to_page)
+        to_page = max_page_number
 
     return from_page, to_page
 
 
-def scrape_books(is_save=True, from_page=None, to_page=None):
+def scrape_books(is_save=True, from_page=None, to_page=None) -> list:
+    """
+    Собирает данные о книгах с сайта https://books.toscrape.com
+    Функция проходит по страницам каталога книг (либо по всем доступным,
+    либо в указанном диапазоне), извлекает ссылки на книги, получает
+    данные по каждой книге и формирует список результатов. При
+    необходимости данные
+    могут быть сохранены во внешний файл.
+    Args:
+        is_save (bool, optional): Если True, результат будет сохранён
+            в файл books_data.txt. По умолчанию True.
+        from_page (int | None, optional): Номер первой страницы для анализа.
+            Если None, обработка начинается с первой доступной страницы.
+        to_page (int | None, optional): Номер последней страницы для анализа.
+            Если None, обработка ведётся до последней доступной страницы.
+    Returns:
+        list: Список словарей, где каждый словарь содержит данные о книгах.
+    """
     about_books = []
     interval = get_interval(from_page, to_page)
-    for N in range(interval[0], interval[1] + 1):
-        url_teml = f"{URL_PREFIX}page-{N}.html"
-        list_books = get_books_urls(url_teml)
+    for page_num in range(interval[0], interval[1] + 1):
+        url = f"{URL_PREFIX}page-{page_num}.html"
+        list_books = get_books_urls(url)
         for book_url in list_books:
             try:
                 about_books.append(get_book_data(book_url))
@@ -116,17 +215,28 @@ def scrape_books(is_save=True, from_page=None, to_page=None):
         write_to_file(about_books)
     return about_books
 
+
 def scrape_by_schedule(poll_interval_seconds: int = 50) -> None:
+    """
+    Периодически запускает процесс сбора данных о книгах (`scrape_books`)
+    по расписанию каждый день в 19:00.
+    Args:
+        poll_interval_seconds (int, optional): Интервал между запусками
+            процесса в секундах. По умолчанию 50 секунд.
+    """
     # Запланировать запуск каждый день в 19:00 по локальному времени машины
     schedule.every().day.at("19:00").do(scrape_books)
-    print("Планировщик запущен. Ожидаю 19:00 каждый день... (Ctrl+C для остановки)")
+    print("Планировщик запущен. Ожидаю 19:00 каждый день... "
+          "(Ctrl+C для остановки)")
     try:
         while True:
             schedule.run_pending()
-            time.sleep(poll_interval_seconds)  # проверяем расписание не постоянно, а раз в N секунд
+            # проверяем расписание не постоянно, а раз в N секунд
+            time.sleep(poll_interval_seconds)
     except KeyboardInterrupt:
         print("\n Остановлено пользователем.")
 
+
 if __name__ == "__main__":
-    scrape_books(from_page=50)
-    #scrape_by_schedule()
+    scrape_books(from_page=50, to_page=51)
+    # scrape_by_schedule()
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -5,38 +5,61 @@ from pathlib import Path
 parent_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(parent_dir))
 
-#from ..scraper import (get_book_data, get_books_urls,
-#                     get_page_counts, get_interval, scrape_books)
 from scraper import (get_interval, get_books_urls,
                      get_page_counts, get_book_data, scrape_books)
 
-#from ..smth import line_eq
-
-# Это вместо глобальных переменных, можно данные раздавать
-@pytest.fixture
-def create_fixture():
-    return [1, 2, 3, 4, 5, 7]
-
 
 def test_interval():
-    assert get_interval(from_page=1, to_page=1) == (1, 1), "ага"
+    assert get_interval(from_page=1, to_page=1) == (1, 1), \
+        "Не верно определен интервал"
+
+    
+def test_interval_exception():
+    with pytest.raises(ValueError):
+        get_interval(from_page=50, to_page=51)
+
+    with pytest.raises(ValueError):
+        get_interval(from_page=2, to_page=1)
 
 
 def test_get_books_urls():
-    book_catalogue_url = "https://books.toscrape.com/catalogue/page-1.html"
-    assert len(get_books_urls(book_catalogue_url)) == 20, "ага"
+    book_catalogue_url = ("https://books.toscrape.com"
+                          "/catalogue/page-1.html")
+    print(get_books_urls(book_catalogue_url))
+    assert len(get_books_urls(book_catalogue_url)) == 20, \
+        "Не верное число ссылок на страницы с информации о книгах"
 
 
 def test_get_page_counts():
-    assert get_page_counts() == 50, "aua"
+    assert get_page_counts() == 50, "Не верное число разделов"
 
 
-def test_get_book_data():
-    url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-    assert len(get_book_data(url)) == 12, "gg"
+def test_get_book_field():
+    url = ("https://books.toscrape.com/catalogue"
+           "/a-light-in-the-attic_1000/index.html")
+    res = get_book_data(url)
+    assert res["Title"] == "A Light in the Attic", \
+        "Отсутствует название книги"
+
+
+def test_get_book_total_info():
+    url = ("https://books.toscrape.com/catalogue"
+           "/a-light-in-the-attic_1000/index.html")
+    assert len(get_book_data(url)) == 11, \
+        "Не верное число параметров страницы"
+
+
+def test_get_book_info():
+    url = ("https://books.toscrape.com/catalogue"
+           "/a-light-in-the-attic_1000/index.html")
+    res = get_book_data(url)
+    assert len(res["Title"]) > 0, "Отсутствует Title"
+    assert len(res["Price"]) > 0, "Отсутствует Price"
+    assert len(res["Description"]) > 0, "Отсутствует Description"
+    assert res["Rating"] is not None, "Отсутствует Rating"
+    assert res["Availability"] is not None, "Отсутствует Availability"
 
 
 def test_scrape_books():
-
-    print(scrape_books(is_save=False, from_page=2, to_page=2))
-    #assert scrape_books(is_save=False, from_page=2, to_page=2) == [1, 2, 3, 4, 5, 7]
+    assert len(scrape_books(is_save=False, from_page=2, to_page=3)) == 40, \
+        "Не верное число"
