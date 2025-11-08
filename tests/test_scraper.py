083996c9d613e1243a038dc739bbd0e39958d43f
JeffRaiAlt <workbifitfokin@gmail.com>
2025-11-08 11:54:21 +0300

    Рефакторинг: - убрал дублирующийся код в тестах - заменил использование requests на request.session - изменил сигнатуру и название метода get_books_url на parse_books_urls - добавил минимальный вывод на консоль информации о ходе обработки

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,11 +1,13 @@
-import pytest
 import sys
 from pathlib import Path
+
+import pytest
+import requests
 
 parent_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(parent_dir))
 
-from scraper import (get_interval, get_books_urls,
+from scraper import (get_interval, parse_books_urls,
                      get_page_counts, get_book_data, scrape_books)
 
 
@@ -13,8 +15,6 @@ def test_interval():
     assert get_interval(from_page=1, to_page=1) == (1, 1), \
         "Не верно определен интервал"
 
-
-def test_interval_exception():
     with pytest.raises(ValueError):
         get_interval(from_page=50, to_page=51)
 
@@ -22,11 +22,11 @@ def test_interval_exception():
         get_interval(from_page=2, to_page=1)
 
 
-def test_get_books_urls():
+def test_parse_books_urls():
     book_catalogue_url = ("https://books.toscrape.com"
                           "/catalogue/page-1.html")
-    print(get_books_urls(book_catalogue_url))
-    assert len(get_books_urls(book_catalogue_url)) == 20, \
+    response = requests.get(book_catalogue_url)
+    assert len(parse_books_urls(response.text)) == 20, \
         "Не верное число ссылок на страницы с информации о книгах"
 
 
@@ -34,31 +34,20 @@ def test_get_page_counts():
     assert get_page_counts() == 50, "Не верное число разделов"
 
 
-def test_get_book_field():
+def test_get_book_data():
     url = ("https://books.toscrape.com/catalogue"
            "/a-light-in-the-attic_1000/index.html")
-    res = get_book_data(url)
-    assert res["Title"] == "A Light in the Attic", \
+    response = get_book_data(url)
+    assert len(response) == 11, "Не верное число параметров страницы"
+
+    assert len(response["Title"]) > 0, "Отсутствует Title"
+    assert len(response["Price"]) > 0, "Отсутствует Price"
+    assert len(response["Description"]) > 0, "Отсутствует Description"
+    assert response["Rating"] is not None, "Отсутствует Rating"
+    assert response["Availability"] is not None, "Отсутствует Availability"
+
+    assert response["Title"] == "A Light in the Attic", \
         "Отсутствует название книги"
-
-
-def test_get_book_total_info():
-    url = ("https://books.toscrape.com/catalogue"
-           "/a-light-in-the-attic_1000/index.html")
-    assert len(get_book_data(url)) == 11, \
-        "Не верное число параметров страницы"
-
-
-def test_get_book_info():
-    url = ("https://books.toscrape.com/catalogue"
-           "/a-light-in-the-attic_1000/index.html")
-    res = get_book_data(url)
-    assert len(res["Title"]) > 0, "Отсутствует Title"
-    assert len(res["Price"]) > 0, "Отсутствует Price"
-    assert len(res["Description"]) > 0, "Отсутствует Description"
-    assert res["Rating"] is not None, "Отсутствует Rating"
-    assert res["Availability"] is not None, "Отсутствует Availability"
-
 
 def test_scrape_books():
     assert len(scrape_books(is_save=False, from_page=2, to_page=3)) == 40, \
